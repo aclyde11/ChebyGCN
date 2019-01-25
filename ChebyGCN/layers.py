e7e7d6a67abf01b574ddfe9be24e115322c39f40
austin <aclyde11@gmail.com>
2019-01-25 11:30:35 -0600

    Init Code

--- ChebyGCN/layers.py
+++ ChebyGCN/layers.py
@@ -54,7 +54,7 @@ class GraphConvolution(Layer):
             self.bias = self.add_weight(name='bias', shape=(1, 1, self.F_1), initializer='uniform', trainable=True)
         super(GraphConvolution, self).build(input_shape)  # Be sure to call this at the end
 
-    def rescale_L(L, lmax=2):
+    def rescale_L(self, L, lmax=2):
         """Rescale the Laplacian eigenvalues in [-1,1]."""
         M, M = L.shape
         I = scipy.sparse.identity(M, format='csr', dtype=L.dtype)
@@ -62,7 +62,7 @@ class GraphConvolution(Layer):
         L -= I
         return L
 
-    def chebyshev5(self, x, L, Fout, K):
+    def chebyshev5(self, x, L, Fout, K_coeff):
         N, M, Fin = K.int_shape(x)
         # Rescale Laplacian and store as a TF sparse tensor. Copy to not modify the shared L.
         L = scipy.sparse.csr_matrix(L)
@@ -78,20 +78,20 @@ class GraphConvolution(Layer):
 
         def concat(x, x_):
             x_ = tf.expand_dims(x_, 0)  # 1 x M x Fin*N
-            return tf.concat([x, x_], axis=0)  # K x M x Fin*N
+            return tf.concat([x, x_], axis=0)  # K_coeff x M x Fin*N
 
-        if K > 1:
+        if K_coeff > 1:
             x1 = tf.sparse_tensor_dense_matmul(L, x0)
             x = concat(x, x1)
-        for k in range(2, K):
+        for k in range(2, K_coeff):
             x2 = 2 * tf.sparse_tensor_dense_matmul(L, x1) - x0  # M x Fin*N
             x = concat(x, x2)
             x0, x1 = x1, x2
-        x = tf.reshape(x, [K, M, Fin, -1])  # K x M x Fin x N
-        x = tf.transpose(x, perm=[3, 1, 2, 0])  # N x M x Fin x K
-        x = tf.reshape(x, [-1, Fin * K])  # N*M x Fin*K
-        # Filter: Fin*Fout filters of order K, i.e. one filterbank per feature pair.
-        #  W = self._weight_variable([Fin*K, Fout], regularization=False)
+        x = tf.reshape(x, [K_coeff, M, Fin, -1])  # K_coeff x M x Fin x N
+        x = tf.transpose(x, perm=[3, 1, 2, 0])  # N x M x Fin x K_coeff
+        x = tf.reshape(x, [-1, Fin * K_coeff])  # N*M x Fin*K_coeff
+        # Filter: Fin*Fout filters of order K_coeff, i.e. one filterbank per feature pair.
+        #  W = self._weight_variable([Fin*K_coeff, Fout], regularization=False)
         x = tf.matmul(x, self.kernel)  # N*M x Fout
         return tf.reshape(x, [-1, M, Fout])  # N x M x Fout
 
